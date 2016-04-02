7b1000e1da5e332ee3be3fa1935b3c9c8c8fe737
Joseph Tinoco <jose.tinoco@gmail.com>
2016-04-02 16:19:09 -0400

    Initial commit (fork, reference conversion from VS to VSCode format, transpiler configs)

--- Box2D-Platformer/Scripts/controls.ts
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="config.ts" />
-/// <reference path="box2d.ts" />
-module controls {
-    export var jumping: boolean = false;
-    export var left: boolean = false;
-    export var right: boolean = false;
-    export var rTally: number = 0;
-    export var lTally: number = 0;
-} 
